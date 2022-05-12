6dc554db6824d41e330e6324eb7b1820bf2b04fe
vladdeveloper1 <105424395+vladdeveloper1@users.noreply.github.com>
2022-05-12 13:48:26 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-document.querySelector('.hamburger').onclick = function() {
-    this.classList.toggle('hamburger_activ');
-    document.querySelector('.menu').classList.toggle('menu_activ');
+window.addEventListener('DOMContentLoaded', () => {
+    let hamburger = document.querySelector('.hamburger');
+    let menu = document.querySelector('.menu');
+    let menuItem = document.querySelectorAll('.menu_item');
+    
+    hamburger.onclick = function () {
+        this.classList.toggle('hamburger_activ');
+        menu.classList.toggle('menu_activ');
     }
+
+    menuItem.forEach(item => {
+        item.onclick = () => {
+            hamburger.classList.toggle('hamburger_activ');
+            menu.classList.toggle('menu_activ');
+        }
+    })
+
+})
